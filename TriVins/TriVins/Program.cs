63c21c1a0fffbbe6f76dda01253a0d7ff5f5cdab
AbdouBenhamideche <abderrahmanebenhamideche@gmail.com>
2024-03-03 17:02:57 -0500

    p

--- TriVins/TriVins/Program.cs
+++ TriVins/TriVins/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TriVins;
 
-namespace TriVins
+// Chemin vers votre fichier CSV
+string filePath = @"DONNE\samples_reduced\sample_01.csv";
+
+Console.WriteLine(File.Exists(filePath));
+Console.ReadKey();
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
 {
-    internal class Program
+    Delimiter = ";",
+    HasHeaderRecord = true
+};
+
+
+using (var reader = new StreamReader(filePath)) 
+using (var csv = new CsvReader(reader, config))
+
+{
+    var records = csv.GetRecords<Vin>().ToList();
+
+    foreach (var record in records)
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine($"Alcohol: {record.alcohol}, Sulphates: {record.sulphates}, Citric Acid: {record.citricacid}, Volatile Acidity: {record.volatileacidity}, Quality: {record.quality}");
     }
+    Console.ReadKey();
+
 }
---
+++ TriVins/TriVins/Vin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriVins
+{
+    internal class Vin
+    {
+        public double alcohol { get; set; }
+        public double sulphates { get; set; }
+        public double citricacid { get; set; }
+        public double volatileacidity { get; set; }
+        public int quality { get; set; }
+
+  
+        public Vin() { }
+    }
+}
